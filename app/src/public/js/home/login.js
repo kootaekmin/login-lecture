b7c47f9fe7450f34e0f4930e5bbfb3befbb4d98a
DESKTOP-K5SD3VR\koota <kootaekmin@gmail.com>
2021-03-07 10:11:29 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,22 @@
 'use strict';
 
-console.log('asdfasdf');
-console.log('sdfgsdfg');
+const id = document.querySelector('#id');
+const psword = document.querySelector('#pw');
+const loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+
+  fetch('/login', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  });
+}
